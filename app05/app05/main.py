6c49e61c895acfa1607e1dc865396a0a43971b56
ValeriaRobledoHernandez <robledohernandezvaleria@cetis50cdmx.com>
2024-09-30 18:28:19 -0600

    se sive la version1.1

--- app05/app05/main.py
+++ app05/app05/main.py
@@ -1,7 +1,62 @@
 import flet as ft
-from flet_core.types import WEB_BROWSER
 
-
+def calcular_imc(textPeso,txtAltura,lblIMC,page):
+    try:
+        peso=float(txtPeso.value)
+        altura=float(txtAltura.value)
+        imc=peso/(altura**2)
+        lblIMC.value=f"Tu IMC es de: {imc:.2f}"
+        page.update()
+        
+    #cerrar el cuadro d dialogo
+    def cerrar_dialogo():
+    
+        page.dialog.open=False
+        page.update()    
+    #validacion de IMC
+        if imc<18.5:
+            dialog=ft.AlertDialog(
+                title="Bajo de peso",
+                content="Tu IMC indica que tienes bajo peso",
+                actions=[
+                    ft.TextButton(text="Cerrar", on_click=cerrar_dialogo)
+                ]
+            )
+        elif imc>=18.5 and imc<24.9:
+            dialog=ft.AlertDialog(
+                title="Peso normal",
+                content="Tu IMC indica que tienes peso normal",
+                actions=[
+                    ft.TextButton(text="Cerrar", on_click=cerrar_dialogo)
+                ]
+            )
+        elif imc>=25 and imc<30:
+            dialog=ft.AlertDialog(
+                title="Sobrepeso",
+                content="Tu IMC indica que tienes sobrepeso ",
+                actions=[
+                    ft.TextButton(text="Cerrar", on_click=cerrar_dialogo)
+                ]
+            )
+        else:
+            dialog=ft.AlertDialog(
+                title="Obesidad",
+                content="Tu IMC indica que tienes Obesidad, acude a tu médico",
+                actions=[
+                    ft.TextButton(text="Cerrar", on_click=cerrar_dialogo)
+                ]
+            )
+        page.dialog=dialog
+        page.dialog.open=True
+        page.update()
+        
+    except ValueError:
+        page.dialog.open=False
+        page.update()
+        
+            
+        
+        
 def main(page: ft.Page):
     page.title="Calculadora de IMC"
     page.bgcolor="purple"
@@ -14,8 +69,18 @@ def main(page: ft.Page):
         height=200,
         fit=ft.ImageFit.CONTAIN
     )
-    btnCalcular=ft.ElevatedButton(text="Calcular")#,on_click=calcular)
-    btnLimpiar=ft.ElevatedButton(text="Limpiar")
+    def on_calcular(event):
+        calcular_imc(txtPeso,txtAltura,lblIMC,page)
+        
+    def limpiar(e):
+        txtPeso.value=""
+        txtAltura.value=""
+        lblIMC.value="Tu IMC es de: "
+        page.update()
+    
+    
+    
+    btnCalcular=ft.ElevatedButton(text="Calcular")
     page.add(
         ft.Column(
             controls=[
